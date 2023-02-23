e946317a7c74c462ffd8fa9a207d0720085cd875
Karthik Mahesh <karthikmahesh@google.com>
2023-02-22 16:23:57 -0800

    Read downloaded data in sample service.
    Bug: 263493591
    Test: manual test
    Change-Id: Ie696ede1a1e1317813bc80a52f68f63abf557ea5

--- tests/endtoendtests/OdpSampleNetwork/src/com/android/odpsamplenetwork/SamplePersonalizationService.java
+++ tests/endtoendtests/OdpSampleNetwork/src/com/android/odpsamplenetwork/SamplePersonalizationService.java
@@ -21,24 +21,48 @@ import android.ondevicepersonalization.DownloadInput;
 import android.ondevicepersonalization.DownloadResult;
 import android.ondevicepersonalization.OnDevicePersonalizationContext;
 import android.ondevicepersonalization.PersonalizationService;
+import android.ondevicepersonalization.RemoteData;
 import android.ondevicepersonalization.RenderContentInput;
 import android.ondevicepersonalization.RenderContentResult;
 import android.ondevicepersonalization.ScoredBid;
 import android.ondevicepersonalization.SelectContentInput;
 import android.ondevicepersonalization.SelectContentResult;
 import android.ondevicepersonalization.SlotResult;
+import android.os.OutcomeReceiver;
 import android.os.ParcelFileDescriptor;
+import android.os.Process;
+import android.os.StrictMode;
+import android.os.StrictMode.ThreadPolicy;
 import android.util.JsonReader;
 import android.util.Log;
+
+import androidx.concurrent.futures.CallbackToFutureAdapter;
+
+import com.google.common.util.concurrent.FluentFuture;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import java.util.function.Consumer;
 
 public class SamplePersonalizationService extends PersonalizationService {
     public final String TAG = "SamplePersonalizationService";
+    public static final int EVENT_TYPE_IMPRESSION = 1;
+    public static final int EVENT_TYPE_CLICK = 2;
+    private static final ListeningExecutorService sBackgroundExecutor =
+            MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(
+                    /* nThreads */ 4,
+                    createThreadFactory("BG Thread", Process.THREAD_PRIORITY_BACKGROUND,
+                            Optional.of(getIoThreadPolicy()))));
 
     @Override
     public void onDownload(
@@ -58,23 +82,137 @@ public class SamplePersonalizationService extends PersonalizationService {
             @NonNull OnDevicePersonalizationContext odpContext,
             @NonNull Consumer<SelectContentResult> consumer
     ) {
-        Log.d(TAG, "onAppRequest() started.");
-        SlotResult.Builder slotResultBuilder = new SlotResult.Builder();
-        slotResultBuilder.addWinningBids(
-                new ScoredBid.Builder()
-                        .setBidId("winningbid1").setPrice(5.0).setScore(10.0).build());
-        slotResultBuilder.addRejectedBids(
-                new ScoredBid.Builder()
-                        .setBidId("losingbid1").setPrice(1.0).setScore(1.0).build());
-        SelectContentResult result =
-                new SelectContentResult.Builder()
-                        .addSlotResults(slotResultBuilder.build())
-                        .build();
-        Log.d(TAG, "onAppRequest() finished.");
-        consumer.accept(result);
+        Log.d(TAG, "selectContent() started.");
+        sBackgroundExecutor.execute(() -> handleSelectContentRequest(input, odpContext, consumer));
     }
 
     @Override public void renderContent(
+            @NonNull RenderContentInput input,
+            @NonNull OnDevicePersonalizationContext odpContext,
+            @NonNull Consumer<RenderContentResult> consumer
+    ) {
+        Log.d(TAG, "renderContent() started.");
+        sBackgroundExecutor.execute(() -> handleRenderContentRequest(input, odpContext, consumer));
+    }
+
+    private ListenableFuture<Map<String, byte[]>> readRemoteData(
+            RemoteData remoteData, List<String> keys) {
+        return CallbackToFutureAdapter.getFuture(completer -> {
+            remoteData.lookup(
+                    keys,
+                    sBackgroundExecutor,
+                    new OutcomeReceiver<Map<String, byte[]>, Exception>() {
+                        @Override public void onResult(Map<String, byte[]> result) {
+                            completer.set(result);
+                        }
+                        @Override public void onError(Exception e) {
+                            completer.setException(e);
+                        }
+                    });
+            return "readRemoteData";
+        });
+    }
+
+    FluentFuture<Integer> getNumAds(RemoteData remoteData) {
+        Log.d(TAG, "getNumAds() called.");
+        return FluentFuture.from(readRemoteData(remoteData, List.of("numads")))
+                .transform(
+                    result -> Integer.parseInt(new String(result.get("numads"))),
+                    sBackgroundExecutor
+                );
+    }
+
+    FluentFuture<List<Ad>> readAds(RemoteData remoteData) {
+        Log.d(TAG, "readAds() called.");
+        return FluentFuture.from(readRemoteData(remoteData, List.of("numads")))
+                .transformAsync(
+                    result -> {
+                        int numAds = Integer.parseInt(new String(result.get("numads")));
+                        ArrayList<String> keys = new ArrayList<>();
+                        for (int i = 1; i <= numAds; ++i) {
+                            keys.add("ad" + i);
+                        }
+                        return readRemoteData(remoteData, keys);
+                    },
+                    sBackgroundExecutor
+                )
+                .transform(
+                    result -> {
+                        ArrayList<Ad> ads = new ArrayList<>();
+                        for (var entry: result.entrySet()) {
+                            Ad ad = parseAd(entry.getKey(), entry.getValue());
+                            if (ad != null) {
+                                ads.add(ad);
+                            }
+                        }
+                        return ads;
+                    },
+                    sBackgroundExecutor
+                );
+    }
+
+    List<Ad> filterAds(List<Ad> ads, SelectContentInput input) {
+        Log.d(TAG, "filterAds() called.");
+        // TODO(b/263493591): Implement match logic.
+        return ads;
+    }
+
+    Ad runAuction(List<Ad> ads) {
+        Log.d(TAG, "runAuction() called.");
+        // TODO(b/263493591): Implement auction logic.
+        return ads.get(0);
+    }
+
+    SelectContentResult buildResult(Ad ad) {
+        Log.d(TAG, "buildResult() called.");
+        return new SelectContentResult.Builder()
+                .addSlotResults(
+                    new SlotResult.Builder()
+                        .addWinningBids(
+                            new ScoredBid.Builder()
+                                .setBidId(ad.mId)
+                                .setPrice(ad.mPrice)
+                                .setScore(ad.mPrice * 10)
+                                .setEventsWithMetrics(EVENT_TYPE_CLICK)
+                                .build())
+                        .build())
+                .build();
+    }
+
+    private void handleSelectContentRequest(
+            @NonNull SelectContentInput input,
+            @NonNull OnDevicePersonalizationContext odpContext,
+            @NonNull Consumer<SelectContentResult> consumer
+    ) {
+        try {
+            RemoteData remoteData = odpContext.getRemoteData();
+
+            var unused = readAds(remoteData)
+                    .transform(
+                        ads -> buildResult(runAuction(filterAds(ads, input))),
+                        sBackgroundExecutor)
+                    .transform(
+                        result -> {
+                            consumer.accept(result);
+                            return null;
+                        },
+                        MoreExecutors.directExecutor())
+                    .catching(
+                        Exception.class,
+                        e -> {
+                            Log.e(TAG, "Execution failed.", e);
+                            consumer.accept(null);
+                            return null;
+                        },
+                        MoreExecutors.directExecutor());
+
+        } catch (Exception e) {
+            Log.e(TAG, "handleSelectContentRequest() failed", e);
+            consumer.accept(null);
+        }
+    }
+
+    public void handleRenderContentRequest(
             @NonNull RenderContentInput input,
             @NonNull OnDevicePersonalizationContext odpContext,
             @NonNull Consumer<RenderContentResult> consumer
@@ -122,5 +260,62 @@ public class SamplePersonalizationService extends PersonalizationService {
             Log.e(TAG, "Failed to parse downloaded data from fd");
         }
         return filteredKeys;
+    }
+
+    private static ThreadFactory createThreadFactory(
+            final String name, final int priority,
+            final Optional<StrictMode.ThreadPolicy> policy) {
+        return new ThreadFactoryBuilder()
+            .setDaemon(true)
+            .setNameFormat(name + " #%d")
+            .setThreadFactory(
+                    new ThreadFactory() {
+                        @Override
+                        public Thread newThread(final Runnable runnable) {
+                            return new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (policy.isPresent()) {
+                                        StrictMode.setThreadPolicy(policy.get());
+                                    }
+                                    // Process class operates on the current thread.
+                                    Process.setThreadPriority(priority);
+                                    runnable.run();
+                                }
+                            });
+                        }
+                    })
+            .build();
+    }
+
+    static class Ad {
+        final String mId;
+        final double mPrice;
+        final String mTargetKeyword;
+        final String mExcludeKeyword;
+        final String mLandingPage;
+        Ad(String id, double price, String targetKeyword, String excludeKeyword,
+                String landingPage) {
+            mId = id;
+            mPrice = price;
+            mTargetKeyword = targetKeyword;
+            mExcludeKeyword = excludeKeyword;
+            mLandingPage = landingPage;
+        }
+    }
+
+    Ad parseAd(String id, byte[] data) {
+        Log.d(TAG, "parseAd: " + id + " " + new String(data));
+        // TODO(b/263493591): Parse JSON ad.
+        return new Ad(id, 1.0, "", "", "");
+    }
+
+    private static ThreadPolicy getIoThreadPolicy() {
+        return new ThreadPolicy.Builder()
+                .detectNetwork()
+                .detectResourceMismatches()
+                .detectUnbufferedIo()
+                .penaltyLog()
+                .build();
     }
 }
