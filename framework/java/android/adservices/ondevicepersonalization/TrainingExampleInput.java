64f58a9e6757ea068933dc158a6440570f3d68d0
Karthik Mahesh <karthikmahesh@google.com>
2023-11-07 14:01:53 -0800

    Hide 'parcelable' from top-level API input class.
    Bug: 308797371
    Test: TH
    Change-Id: I35260eb225119fb809997052e958cc35ae8aef4d

--- framework/java/android/adservices/ondevicepersonalization/TrainingExampleInput.java
+++ framework/java/android/adservices/ondevicepersonalization/TrainingExampleInput.java
@@ -18,20 +18,17 @@ package android.adservices.ondevicepersonalization;
 
 import android.annotation.NonNull;
 import android.annotation.Nullable;
-import android.os.Parcelable;
 
 import com.android.ondevicepersonalization.internal.util.AnnotationValidations;
 import com.android.ondevicepersonalization.internal.util.DataClass;
-
-import java.util.function.Consumer;
 
 /**
  * The input data for {@link IsolatedWorker#onTrainingExample(TrainingExampleInput, Consumer)}
  *
  * @hide
  */
-@DataClass(genHiddenBuilder = true, genEqualsHashCode = true)
-public final class TrainingExampleInput implements Parcelable {
+@DataClass(genBuilder = false, genHiddenConstructor = true, genEqualsHashCode = true)
+public final class TrainingExampleInput {
     /** The name of the federated compute population. */
     @NonNull private String mPopulationName = "";
 
@@ -43,6 +40,11 @@ public final class TrainingExampleInput implements Parcelable {
 
     /** Token used to support the resumption of training. */
     @Nullable private byte[] mResumptionToken = null;
+
+    /** @hide */
+    public TrainingExampleInput(@NonNull TrainingExampleInputParcel parcel) {
+        this(parcel.getPopulationName(), parcel.getTaskName(), parcel.getResumptionToken());
+    }
 
 
 
@@ -59,8 +61,20 @@ public final class TrainingExampleInput implements Parcelable {
     //@formatter:off
 
 
+    /**
+     * Creates a new TrainingExampleInput.
+     *
+     * @param populationName
+     *   The name of the federated compute population.
+     * @param taskName
+     *   The name of the task within the population. One population may have multiple tasks.
+     *   The task name can be used to uniquely identify the job.
+     * @param resumptionToken
+     *   Token used to support the resumption of training.
+     * @hide
+     */
     @DataClass.Generated.Member
-    /* package-private */ TrainingExampleInput(
+    public TrainingExampleInput(
             @NonNull String populationName,
             @NonNull String taskName,
             @Nullable byte[] resumptionToken) {
@@ -131,142 +145,11 @@ public final class TrainingExampleInput implements Parcelable {
         return _hash;
     }
 
-    @Override
-    @DataClass.Generated.Member
-    public void writeToParcel(@NonNull android.os.Parcel dest, int flags) {
-        // You can override field parcelling by defining methods like:
-        // void parcelFieldName(Parcel dest, int flags) { ... }
-
-        dest.writeString(mPopulationName);
-        dest.writeString(mTaskName);
-        dest.writeByteArray(mResumptionToken);
-    }
-
-    @Override
-    @DataClass.Generated.Member
-    public int describeContents() { return 0; }
-
-    /** @hide */
-    @SuppressWarnings({"unchecked", "RedundantCast"})
-    @DataClass.Generated.Member
-    /* package-private */ TrainingExampleInput(@NonNull android.os.Parcel in) {
-        // You can override field unparcelling by defining methods like:
-        // static FieldType unparcelFieldName(Parcel in) { ... }
-
-        String populationName = in.readString();
-        String taskName = in.readString();
-        byte[] resumptionToken = in.createByteArray();
-
-        this.mPopulationName = populationName;
-        AnnotationValidations.validate(
-                NonNull.class, null, mPopulationName);
-        this.mTaskName = taskName;
-        AnnotationValidations.validate(
-                NonNull.class, null, mTaskName);
-        this.mResumptionToken = resumptionToken;
-
-        // onConstructed(); // You can define this method to get a callback
-    }
-
-    @DataClass.Generated.Member
-    public static final @NonNull Parcelable.Creator<TrainingExampleInput> CREATOR
-            = new Parcelable.Creator<TrainingExampleInput>() {
-        @Override
-        public TrainingExampleInput[] newArray(int size) {
-            return new TrainingExampleInput[size];
-        }
-
-        @Override
-        public TrainingExampleInput createFromParcel(@NonNull android.os.Parcel in) {
-            return new TrainingExampleInput(in);
-        }
-    };
-
-    /**
-     * A builder for {@link TrainingExampleInput}
-     * @hide
-     */
-    @SuppressWarnings("WeakerAccess")
-    @DataClass.Generated.Member
-    public static final class Builder {
-
-        private @NonNull String mPopulationName;
-        private @NonNull String mTaskName;
-        private @Nullable byte[] mResumptionToken;
-
-        private long mBuilderFieldsSet = 0L;
-
-        public Builder() {
-        }
-
-        /**
-         * The name of the federated compute population.
-         */
-        @DataClass.Generated.Member
-        public @NonNull Builder setPopulationName(@NonNull String value) {
-            checkNotUsed();
-            mBuilderFieldsSet |= 0x1;
-            mPopulationName = value;
-            return this;
-        }
-
-        /**
-         * The name of the task within the population. One population may have multiple tasks.
-         * The task name can be used to uniquely identify the job.
-         */
-        @DataClass.Generated.Member
-        public @NonNull Builder setTaskName(@NonNull String value) {
-            checkNotUsed();
-            mBuilderFieldsSet |= 0x2;
-            mTaskName = value;
-            return this;
-        }
-
-        /**
-         * Token used to support the resumption of training.
-         */
-        @DataClass.Generated.Member
-        public @NonNull Builder setResumptionToken(@NonNull byte... value) {
-            checkNotUsed();
-            mBuilderFieldsSet |= 0x4;
-            mResumptionToken = value;
-            return this;
-        }
-
-        /** Builds the instance. This builder should not be touched after calling this! */
-        public @NonNull TrainingExampleInput build() {
-            checkNotUsed();
-            mBuilderFieldsSet |= 0x8; // Mark builder used
-
-            if ((mBuilderFieldsSet & 0x1) == 0) {
-                mPopulationName = "";
-            }
-            if ((mBuilderFieldsSet & 0x2) == 0) {
-                mTaskName = "";
-            }
-            if ((mBuilderFieldsSet & 0x4) == 0) {
-                mResumptionToken = null;
-            }
-            TrainingExampleInput o = new TrainingExampleInput(
-                    mPopulationName,
-                    mTaskName,
-                    mResumptionToken);
-            return o;
-        }
-
-        private void checkNotUsed() {
-            if ((mBuilderFieldsSet & 0x8) != 0) {
-                throw new IllegalStateException(
-                        "This Builder should not be reused. Use a new Builder instance instead");
-            }
-        }
-    }
-
     @DataClass.Generated(
-            time = 1697577073626L,
+            time = 1699394018457L,
             codegenVersion = "1.0.23",
             sourceFile = "packages/modules/OnDevicePersonalization/framework/java/android/adservices/ondevicepersonalization/TrainingExampleInput.java",
-            inputSignatures = "private @android.annotation.NonNull java.lang.String mPopulationName\nprivate @android.annotation.NonNull java.lang.String mTaskName\nprivate @android.annotation.Nullable byte[] mResumptionToken\nclass TrainingExampleInput extends java.lang.Object implements [android.os.Parcelable]\n@com.android.ondevicepersonalization.internal.util.DataClass(genHiddenBuilder=true, genEqualsHashCode=true)")
+            inputSignatures = "private @android.annotation.NonNull java.lang.String mPopulationName\nprivate @android.annotation.NonNull java.lang.String mTaskName\nprivate @android.annotation.Nullable byte[] mResumptionToken\nclass TrainingExampleInput extends java.lang.Object implements []\n@com.android.ondevicepersonalization.internal.util.DataClass(genBuilder=false, genHiddenConstructor=true, genEqualsHashCode=true)")
     @Deprecated
     private void __metadata() {}
 
